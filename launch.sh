ee40f11dfcda639db1993a199bba198575fe672a
tkeymp <57038569+tkeymp@users.noreply.github.com>
2019-11-10 20:30:08 +0500

    0.1.1 released!

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 connect=3.15.45.237:8333
 reward_address=qMKSHAeyWvCTLb9tBzES5tbPMgantChnMh
 
-screen -dmS pool_client ./pool_client $connect $reward_address
+screen -L -dmS pool_client sudo ./pool_client $connect $reward_address
 echo Use \"screen -r pool_client\" for attach to pool_client screen
